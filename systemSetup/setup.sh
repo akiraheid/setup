189343e06a9f862b6440925943ec62786c5cdd06
Akira Heid <mheid2011@gmail.com>
2016-08-27 18:23:58 -0400

    Add installation of pathogen to setup.sh

--- systemSetup/setup.sh
+++ systemSetup/setup.sh
@@ -2,8 +2,26 @@
 
 # Perform dev-system set up.
 
+echo "== Updating"
 yes | apt-get update
+
+echo "== Upgrading"
 yes | apt-get upgrade
+
+echo "== Dist-upgrading"
 yes | apt-get dist-upgrade
+
+echo "== Installing dev packages"
 yes | apt-get install clang cmake cppcheck curl git vim
+
+echo "== Autoremove"
 yes | apt-get autoremove
+
+echo "== Checking for pathogen..."
+if [ ! -f ~/.vim/autoload/pathogen.vim ]; then
+  echo "==== Pathogen not found. Installing"
+  mkdir -p ~/.vim/autoload ~/.vim/bundle
+  curl https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim -o ~/.vim/autoload/pathogen.vim
+else
+  echo "==== Pathogen already installed"
+fi
