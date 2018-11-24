fe1fd270574c93e627f000919ddde7e4a94647ce
Akira <mheid2011@gmail.com>
2018-11-24 15:22:19 -0500

    Add npm install

--- configure.sh
+++ configure.sh
@@ -5,115 +5,153 @@
 HOME=$(readlink -f ~)
 VIM_BUNDLE_DIR=~/.vim/bundle
 REPO_DIR="${HOME}/repos"
-TOR_URL="https://www.torproject.org/dist/torbrowser/8.0.1/tor-browser-linux64-8.0.1_en-US.tar.xz"
+SETUP_DIR="${REPO_DIR}/setup"
+
+function checkInstall() {
+	if ! command -v $1; then
+		sudo apt-get install -y $1
+	fi
+}
 
 function updateSystem() {
-    echo "== Updating"
-    sudo apt-get -y update
+	echo "== Updating"
+	sudo apt-get -y update
 
-    echo "== Upgrading"
-    sudo apt-get -y upgrade
+	echo "== Upgrading"
+	sudo apt-get -y upgrade
 
-    echo "== Autoremove"
-    sudo apt-get -y autoremove
+	echo "== Autoremove"
+	sudo apt-get -y autoremove
 
-    echo "== Installing dev packages"
-    sudo apt-get -y install clang cmake cppcheck curl git vim tmux
+	echo "== Installing dev packages"
+	sudo apt-get -y install curl g++ gcc git make tmux vim
 
-    echo "== Autoremove"
-    sudo apt-get -y autoremove
+	echo "== Autoremove"
+	sudo apt-get -y autoremove
 }
 
 function installpathogen() {
-    echo "== Checking for pathogen..."
-    if [ ! -f ~/.vim/autoload/pathogen.vim ]; then
-        echo "=== Pathogen not found. Installing"
-        mkdir -p ~/.vim/autoload ${VIM_BUNDLE_DIR}
-        curl https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim -o ~/.vim/autoload/pathogen.vim
-    else
-        echo "=== Pathogen already installed"
-    fi
+	echo "== Checking for pathogen..."
+	if [ -f ~/.vim/autoload/pathogen.vim ]; then
+		echo "=== Pathogen already installed"
+		return
+	fi
+
+	echo "=== Pathogen not found. Installing"
+	mkdir -p ~/.vim/autoload ${VIM_BUNDLE_DIR}
+	curl https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim -o ~/.vim/autoload/pathogen.vim
 }
 
 function installSyntastic() {
-    echo "== Checking for syntastic..."
-    if [ ! -d "${VIM_BUNDLE_DIR}/syntastic" ]; then
-        echo "=== Syntastic not found. Installing"
-        git clone --depth=1 https://github.com/scrooloose/syntastic.git $VIM_BUNDLE_DIR/syntastic
-    else
-        echo "=== Syntastic already installed"
-    fi
+	echo "== Checking for syntastic..."
+	if [ ! -d "${VIM_BUNDLE_DIR}/syntastic" ]; then
+		echo "=== Syntastic already installed"
+		return
+	fi
+
+	checkInstall git
+	echo "=== Syntastic not found. Installing"
+	git clone --depth=1 https://github.com/scrooloose/syntastic.git $VIM_BUNDLE_DIR/syntastic
+}
+
+function installeslint() {
+	echo "== Checking for eslint..."
+	if [ command -v eslint ]; then
+		echo "=== eslint already installed"
+		return
+	fi
+
+	echo "=== eslint not found. Installing"
+	if ! command -v npm; then
+		installnpm
+	fi
+	npm i -g eslint
 }
 
 function cloneSetup() {
-    echo "== Cloning setup repo"
-    mkdir $REPO_DIR; cd $REPO_DIR
-    git clone https://github.com/akiraheid/setup.git
+	checkInstall git
 
-    SETUP_DIR="${REPO_DIR}/setup"
-    echo "setup repo cloned to $REPO_DIR"
+	echo "== Cloning setup repo"
+	mkdir $REPO_DIR; cd $REPO_DIR
+	git clone https://github.com/akiraheid/setup.git
+	echo "setup repo cloned to $REPO_DIR"
 }
 
 function installbashrc() {
-    echo "== Installing .bashrc..."
-    BASHRC="${HOME}/.bashrc"
-    if [ -f ${BASHRC} ]; then
-        echo "=== .bashrc exists. Creating backup: .bashrc.bak"
-        mv ${BASHRC} "${BASHRC}.bak" >/dev/null 2>&1
-    fi
-    echo "=== Creating symlink to bashrc"
-    ln -s "${SETUP_DIR}/bashrc" ${BASHRC}
+	echo "== Installing .bashrc..."
+	BASHRC="${HOME}/.bashrc"
+	if [ -f ${BASHRC} ]; then
+		echo "=== .bashrc exists. Creating backup: .bashrc.bak"
+		mv ${BASHRC} "${BASHRC}.bak" >/dev/null 2>&1
+	fi
+	echo "=== Creating symlink to bashrc"
+	ln -s "${SETUP_DIR}/bashrc" ${BASHRC}
 }
 
 function installvimrc() {
-    echo "== Installing .vimrc"
-    VIMRC="${HOME}/.vimrc"
-    if [ -f $VIMRC ]; then
-        echo "=== .vimrc exists. Creating backup: .vimrc.bak"
-        mv ${VIMRC} "${VIMRC}.bak" >/dev/null 2>&1
-    fi
-    ln -s "${SETUP_DIR}/vimrc" ${VIMRC}
+	echo "== Installing .vimrc"
+	VIMRC="${HOME}/.vimrc"
+	if [ -f $VIMRC ]; then
+		echo "=== .vimrc exists. Creating backup: .vimrc.bak"
+		mv ${VIMRC} "${VIMRC}.bak" >/dev/null 2>&1
+	fi
+	ln -s "${SETUP_DIR}/vimrc" ${VIMRC}
 }
 
 function installDocker() {
-    echo "== Installing Docker"
-    sudo apt-get remove -y docker docker-engine docker.io
-    echo "=== Install dependencies"
-    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-    echo "=== Add docker repository"
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    echo "=== Install docker"
-    sudo apt-get update; sudo apt-get install -y docker-ce;
-    echo "=== Test docker"
-    sudo docker run hello-world
+	checkInstall curl
+
+	echo "== Installing Docker"
+	sudo apt-get remove -y docker docker-engine docker.io
+	echo "=== Install dependencies"
+	sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+	echo "=== Add docker repository"
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	echo "=== Install docker"
+	sudo apt-get update; sudo apt-get install -y docker-ce;
+	echo "=== Test docker"
+	sudo docker run hello-world
 }
 
 function installTor() {
-    ZIPPED_TOR="~/tmp/tor-browser-8.0.1.tar.xz"
-    UNZIPPED_TOR="~/Download/tor-browser"
-    echo "== Installing Tor"
-    mkdir -p $UNZIPPED_TOR "~/tmp"
-    sudo apt-get install -y curl
-    curl -o $ZIPPED_TOR $TOR_URL
-    tar -xzf $ZIPPED_TOR $UNZIPPED_TOR
-    rm $ZIPPED_TOR
+	TOR_VERSION=8.0.1
+	TOR_URL="https://www.torproject.org/dist/torbrowser/${TOR_VERSION}/tor-browser-linux64-${TOR_VERSION}_en-US.tar.xz"
+	ZIPPED_TOR="~/tmp/tor-browser-${TOR_VERSION}.tar.xz"
+	UNZIPPED_TOR="~/Download/tor-browser"
+
+	checkInstall curl
+
+	echo "== Installing Tor"
+	mkdir -p $UNZIPPED_TOR "~/tmp"
+	curl -o $ZIPPED_TOR $TOR_URL
+	tar -xzf $ZIPPED_TOR $UNZIPPED_TOR
+	rm $ZIPPED_TOR
 }
 
 function installnpm() {
-    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-    sudo apt-get install -y nodejs g++ gcc make
+	echo "== Checking for npm..."
+	if command -v npm; then
+		echo "=== npm already installed"
+		return
+	fi
+
+	echo "=== npm not found. Installing"
+	checkInstall curl
+	curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+	$(nvm install node)
 }
 
 # Comment out options as desired
 echo "== Beginning configuration"
 updateSystem
 cloneSetup
-installpathogen
-installSyntastic
 installbashrc
 installvimrc
+#installpathogen
+#installSyntastic
+#installnpm
+#installeslint
 #installDocker
-installTor
-installnpm
+#installTor
 echo "== Done"
