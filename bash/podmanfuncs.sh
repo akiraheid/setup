2d545325d6e82b145ea301094fc8d78562d566b3
x <x@x.x>
2021-01-01 20:02:23 -0500

    move syncthing command

--- bash/podmanfuncs.sh
+++ bash/podmanfuncs.sh
@@ -2,11 +2,15 @@
 
 # Functions to run podman containers
 
+stopdel() {
+	podman stop -i $1
+	podman rm -i $1
+}
+
 keepassxc() {
 	NAME=keepassxc
 
-	podman stop -i $NAME
-	podman rm -i $NAME
+	stopdel $NAME
 
 	podman run -d --rm \
 		--name $NAME \
@@ -17,4 +21,25 @@ keepassxc() {
 		-v /tmp/.X11-unix:/tmp/.X11-unix \
 		-v /usr/share/X11/xkb/:/usr/share/X11/xkb/:ro \
 		localhost/keepassxc
+}
+
+syncthing() {
+	NAME=syncthing
+
+	stopdel $NAME
+
+	DIR=~/syncthing
+	mkdir -p $DIR
+
+	# normally runs with port mappings -p 8384:8384 -p 22000:22000 but using
+	# --network=host eliminates the need to specify the mappings
+	podman run -d --rm \
+		-e PUID="`id -u`" \
+		-e PGID="`id -g`" \
+		-e STGUIADDRESS="" \
+		--name syncthing \
+		--network=host \
+		--userns=keep-id \
+		--volume $DIR/:/var/syncthing/ \
+		syncthing/syncthing:latest
 }
