d7d0a18797148db49bbf818cde36dd7765f8648b
x <x@x.x>
2021-01-01 19:51:25 -0500

    mount keepassxc db

--- bash/podmanfuncs.sh
+++ bash/podmanfuncs.sh
@@ -12,6 +12,7 @@ keepassxc() {
 		--name $NAME \
 		-e DISPLAY=unix${DISPLAY} \
 		-v ${HOME}/.config/keepassxc/:/root/.config/keepassxc/ \
+		-v ${HOME}/syncthing/computers/keepassxc.kdbx:/root/.config/keepassxc/keepassxc.kdbx \
 		-v /etc/machine-id:/etc/machine-id:ro \
 		-v /tmp/.X11-unix:/tmp/.X11-unix \
 		-v /usr/share/X11/xkb/:/usr/share/X11/xkb/:ro \
