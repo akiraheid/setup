11309f0a3c1dba2722a07574333126a6c7908a06
Akira Heid <akira710@protonmail.com>
2021-01-16 21:32:57 -0500

    fix saving to keepassxc database
    KeepassXC wouldn't save a change to the database because the database
    file was mounted into a directory that was already a mounted directory
    of the configuration file. Putting the configuration file and the
    database file into the same directory and mounting the directory fixes
    the database write problem.

--- bash/podmanfuncs.sh
+++ bash/podmanfuncs.sh
@@ -32,8 +32,7 @@ keepassxc() {
 	podman run -d --rm \
 		--name $NAME \
 		-e DISPLAY=unix${DISPLAY} \
-		-v ${HOME}/.config/keepassxc/:/root/.config/keepassxc/ \
-		-v ${HOME}/syncthing/computers/keepassxc.kdbx:/root/.config/keepassxc/keepassxc.kdbx \
+		-v ${HOME}/syncthing/computers/keepassxc/:/root/.config/keepassxc/ \
 		-v /etc/machine-id:/etc/machine-id:ro \
 		-v /tmp/.X11-unix:/tmp/.X11-unix \
 		-v /usr/share/X11/xkb/:/usr/share/X11/xkb/:ro \
