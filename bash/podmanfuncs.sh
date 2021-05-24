7ccbbfcacd3ff1778cb4610d980c310e3bba9ffd
Akira Heid <akira710@protonmail.com>
2021-05-23 21:06:30 -0400

    update oocalc alias, remove oocalc podman func
    Remove oocalc podman func because it has a binary/installation along
    with the container image.

--- bash/podmanfuncs.sh
+++ bash/podmanfuncs.sh
@@ -29,28 +29,6 @@ gimp() {
 		akiraheid/gimp
 }
 
-openoffice-calc() {
-	NAME=openoffice-calc
-
-	if [ $# != 0 ]; then
-		VOL="-v $(readlink -f $1):/root/data/$(basename $1)"
-		ARG="/root/data/$1"
-	else
-		ARG=""
-	fi
-	podman run -d --rm \
-		-e DISPLAY=unix${DISPLAY} \
-		--name ${NAME} \
-		-v ${HOME}/.config/openoffice:/root/.openoffice:rw \
-		-v /tmp/.X11-unix:/tmp/.X11-unix \
-		-v /usr/share/X11/xkb/:/usr/share/X11/xkb/:ro \
-		$VOL \
-		akiraheid/openoffice-calc $ARG
-}
-oocalc() {
-	openoffice-calc $@
-}
-
 syncthing() {
 	NAME=syncthing
 
