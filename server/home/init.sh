99cd6d7a193eaba29824757923214c2277429960
Rancid Ranger <its@ya.boi>
2022-11-12 23:05:13 -0500

    add polo update to cron

--- server/home/init.sh
+++ server/home/init.sh
@@ -69,17 +69,17 @@ service ssh restart
 
 # ========== FIREWALL ==========
 echo "Setting up firewall"
-fwredirect() {
-	FROM=$1
-	TO=$2
-    echo "  Redirecting ${FROM} => ${TO}..."
-	iptables -A INPUT -i $INTERFACE -p tcp --dport $FROM -j ACCEPT
-	iptables -A INPUT -i $INTERFACE -p udp --dport $FROM -j ACCEPT
-	iptables -A INPUT -i $INTERFACE -p tcp --dport $TO -j ACCEPT
-	iptables -A INPUT -i $INTERFACE -p udp --dport $TO -j ACCEPT
-	iptables -A PREROUTING -t nat -i $INTERFACE -p tcp --dport $FROM -m state --state NEW -j REDIRECT --to-port $TO
-	iptables -A PREROUTING -t nat -i $INTERFACE -p udp --dport $FROM -m state --state NEW -j REDIRECT --to-port $TO
-}
+#fwredirect() {
+#	FROM=$1
+#	TO=$2
+#    echo "  Redirecting ${FROM} => ${TO}..."
+#	iptables -A INPUT -i $INTERFACE -p tcp --dport $FROM -j ACCEPT
+#	iptables -A INPUT -i $INTERFACE -p udp --dport $FROM -j ACCEPT
+#	iptables -A INPUT -i $INTERFACE -p tcp --dport $TO -j ACCEPT
+#	iptables -A INPUT -i $INTERFACE -p udp --dport $TO -j ACCEPT
+#	iptables -A PREROUTING -t nat -i $INTERFACE -p tcp --dport $FROM -m state --state NEW -j REDIRECT --to-port $TO
+#	iptables -A PREROUTING -t nat -i $INTERFACE -p udp --dport $FROM -m state --state NEW -j REDIRECT --to-port $TO
+#}
 
 apt-get install -y ufw
 echo "y" | ufw enable
