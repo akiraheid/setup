f93815202eadd845c546ec18184162a43f223aa8
Rancid Ranger <its@ya.boi>
2024-01-06 19:51:49 -0500

    add podgrab cronjob

---
+++ server/home/podgrab/cron.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+dataDir=/raid/apps/podgrab
+storageDir=/raid/podcasts
+echo "Making hardlinks for $dataDir"
+cd $dataDir
+for dir in */; do
+	dir=${dir%*/} # Remove trailing '/'
+	dir=${dir##*/} # print folder name
+	echo "Processing $dir"
+	mkdir -pv "$storageDir/$dir"
+	pushd "$dir"
+	find -type f -links 1 -exec ln -v {} "$storageDir/$dir/{}" \;
+	popd
+done
--- server/home/podgrab/install.sh
+++ server/home/podgrab/install.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 set -e
 
+DIR=$(dirname `readlink -f $0`)
+
 echo "Install Podgrab..."
 
 name=podgrab
 
-
 echo "Create data dirs..."
-sudo mkdir -pv /raid/apps/podgrab
+dataDir=/raid/apps/podgrab
+sudo mkdir -pv $dataDir
+sudo chown $USER:$USER $dataDir
 echo "Create data dirs... done"
+
+echo "Install cron job..."
+bash $DIR/../installUserCron.sh
+toPath=$HOME/cron/daily/podgrab-link.sh
+ln -v "$DIR/cron.sh" $toPath
+chmod 700 $toPath
+echo "Install cron job... done"
 
 echo "Install Podgrab... done"
