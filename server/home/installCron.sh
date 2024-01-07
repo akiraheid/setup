5391bcd39aec7f82e32720e9abd07c5c11d027a7
Rancid Ranger <its@ya.boi>
2024-01-06 23:34:32 -0500

    add anacron configuration

--- server/home/installCron.sh
+++ server/home/installCron.sh
@@ -9,12 +9,16 @@ date -u
 
 DIR=$(dirname -- $0;)
 
-echo "Create daily cron"
-pushd $DIR/jellyfin
-sudo rsync -c jellyfin-pod.yaml jellyfin-start.sh /etc/cron.daily/
-popd
-echo "Create daily cron... done"
-
-echo "Create weekly cron"
+echo "Create system weekly cron"
 sudo rsync -c $DIR/updateSystem.sh /etc/cron.weekly/
-echo "Create weekly cron... done"
+echo "Create system weekly cron... done"
+
+echo "Create $USER cron folders"
+mkdir -pv $HOME/cron/{daily,hourly,monthly,weekly}
+echo "Create $USER cron folders... done"
+
+echo "Create $USER daily cron"
+mkdir -pv ~/.anacron/{etc,spool}
+ln -v anacrontab $HOME/.anacron/etc/anacrontab
+crontab < cron.jobs
+echo "Create $USER daily cron... done"
