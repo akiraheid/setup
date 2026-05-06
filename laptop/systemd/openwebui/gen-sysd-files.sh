4c95ac83fe45959558c9b5f3ead6c73b4784dcc2
Rock <rock@rock.rock>
2026-05-05 21:33:20 -0400

    laptop: fix overcommiting memory to Ollama
    Reduce Ollama container memory to 1 GB because the GPU VRAM
    shares memory with CPU RAM and the GPU VRAM is set at 90% of system
    memory already (27 GB / 32 GB).
    
    Also restrict memory on the OpenTerminal so it doesn't go crazy.

--- laptop/systemd/openwebui/gen-sysd-files.sh
+++ laptop/systemd/openwebui/gen-sysd-files.sh
@@ -5,31 +5,34 @@ this_dir=$(dirname "$(readlink -f "$0")")
 cd "$this_dir"
 
 echo "Generate Ollama systemd unit file..."
-# --memory is 10g because the machine uses shared memory (RAM + GPU VRAM)
+# --memory is 1g because the machine uses shared memory (RAM + GPU VRAM) and the
+# GPU is already set to use 90% of the memory.
 podman create --name ollama \
 	--cpus=15 \
 	--device /dev/kfd \
 	--device /dev/dri \
-    -e "OLLAMA_CONTEXT_LENGTH=130000" \
+	-e "OLLAMA_CONTEXT_LENGTH=130000" \
 	--group-add keep-groups \
-	--memory=10g \
-	--memory-reservation=2g \
+	--memory=1g \
+	--memory-reservation=500m \
 	-p "11434:11434" \
+	--replace \
 	-v "ollama-data:/root/.ollama:rw" \
-    docker.io/ollama/ollama:0.23.0-rocm
+	docker.io/ollama/ollama:0.23.0-rocm
 
 podman generate systemd --new --name ollama > "ollama.service"
 podman rm ollama
 
 echo "Generate OpenWebUI systemd unit file..."
 podman create --name openwebui \
-    -e "OLLAMA_BASE_URLS=http://host.containers.internal:11434" \
-    -p "8080:8080" \
 	--cpus=2 \
+	-e "OLLAMA_BASE_URLS=http://host.containers.internal:11434" \
+	-p "8080:8080" \
 	--memory=1g \
 	--memory-reservation=500m \
-    -v "openwebui-data:/app/backend/data:rw" \
-    ghcr.io/open-webui/open-webui:v0.9.2
+	--replace \
+	-v "openwebui-data:/app/backend/data:rw" \
+	ghcr.io/open-webui/open-webui:v0.9.2
 
 podman generate systemd --new --name openwebui > "openwebui.service"
 podman rm openwebui
@@ -37,9 +40,13 @@ podman rm openwebui
 echo "Generate OpenTerminal systemd unit file..."
 name=openterminal
 podman create --name "${name}" \
-    -e "OPEN_TERMINAL_API_KEY=MySuperSecretKey294" \
-    -p "8082:8000" \
-    -v "openterminal:/home/user:rw" \
+	--cpus=1 \
+	-e "OPEN_TERMINAL_API_KEY=MySuperSecretKey294" \
+	--memory=500m \
+	--memory-reservation=250m \
+	-p "8082:8000" \
+	--replace \
+	-v "openterminal:/home/user:rw" \
 	ghcr.io/open-webui/open-terminal:0.11.34
 
 podman generate systemd --new --name "${name}" > "${name}.service"
